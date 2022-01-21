4de587fceb17eab598a158c0f99c4bf42b44fe40
Aleksandr Kudashkin <kaleks@gmx.com>
2022-01-21 10:23:57 -0500

    Get interaction with backend

--- app/app.py
+++ app/app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/turn", methods = ['POST'])
+def turn():
+    data = {
+        "status": "ongoing",
+        "board": {
+            0: "_",
+            1: "_",
+            2: "_",
+            3: "_",
+            4: "_",
+            5: "_",
+            6: "_",
+            7: "X",
+            8: "_"
+        }
+    }
+    return jsonify(data)
